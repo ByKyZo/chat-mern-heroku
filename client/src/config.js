f18bfbfc0b9ee9ccf70ab30a536e011969d12710
ByKyZo <narcrom.yt@gmail.com>
2021-04-26 15:57:40 +0200

    react empty api_url /config

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,8 @@
 // export const API_URL = 'http://localhost/api-chat-group-master/';
 
 // NODE JS
-export const API_URL = 'http://localhost:5050/api/';
+// export const API_URL = 'http://localhost:5050/api/';
+export const API_URL = '';
 
-export const SOCKET_URL = 'http://localhost:5050';
+// export const SOCKET_URL = 'http://localhost:5050';
+export const SOCKET_URL = '';
