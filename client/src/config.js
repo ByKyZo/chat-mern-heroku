c34c48c7bdaa11219d455f0bc3db8d5f06821c01
ByKyZo <narcrom.yt@gmail.com>
2021-04-26 16:40:03 +0200

    test

--- client/src/config.js
+++ client/src/config.js
@@ -4,7 +4,7 @@
 // NODE JS
 // export const API_URL = 'http://localhost:5050/api/';
 // export const API_URL = 'https://chat-group-master.herokuapp.com/api/';
-export const API_URL = 'totot';
+export const API_URL = '';
 
 
 // export const SOCKET_URL = 'http://localhost:5050';
